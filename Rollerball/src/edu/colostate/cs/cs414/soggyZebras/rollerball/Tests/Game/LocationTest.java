2620de54023ecaa945ff96a7ae9e5283608f8853
dawsoncanby <dawsoncanby@gmail.com>
2018-10-19 12:29:11 -0600

    added tests for location

--- Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Game/LocationTest.java
+++ Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Game/LocationTest.java
@@ -1,9 +1,12 @@
 package edu.colostate.cs.cs414.soggyZebras.rollerball.Tests.Game;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import edu.colostate.cs.cs414.soggyZebras.rollerball.Game.Location;
+
+import java.util.Locale;
 
 class LocationTest {
 
@@ -19,4 +22,23 @@ class LocationTest {
         assertEquals(l.getCol(), 2);
     }
 
+    @Test
+    void testEqualsT() {
+        assertEquals(new Location(1, 1), new Location(1, 1));
+    }
+
+    @Test
+    void testEqualsF() {
+        assertNotEquals(new Location(1, 1), new Location(1, 2));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("1 1", new Location(1, 1).toString());
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals("1 1".hashCode(), new Location(1, 1).hashCode());
+    }
 }
