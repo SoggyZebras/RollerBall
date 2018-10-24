72476c44f901c27ae8facb23f6f00185544317ca
Jordan-Peterson <jordantp@rams.colostate.edu>
2018-10-23 23:17:15 -0600

    Added some client constructor tests

--- Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Client/ClientTest.java
+++ Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Client/ClientTest.java
@@ -2,13 +2,25 @@ package edu.colostate.cs.cs414.soggyZebras.rollerball.Tests.Client;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import edu.colostate.cs.cs414.soggyZebras.rollerball.Client.Client;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 class ClientTest {
 
 	@Test
-	void test() {
-		assertEquals(true, true);
+	void testConstructorNoArg() {
+
+		//Throw an exception if the Client class was created with no arguments
+		assertThrows(IOException.class, () -> {new Client();});
+	}
+
+	@Test
+	void testConstructorNullArg() throws IOException {
+
+		//Throw exception if the Client object was created wit null a argument(s)
+		Client c = new Client();
 	}
 
 }
