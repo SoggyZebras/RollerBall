4051e201c9e90fb6fc81a9002fb077cdab5db5e0
Jordan-Peterson <jordantp@rams.colostate.edu>
2018-10-23 23:47:06 -0600

    Mostly finished initial client side connection

--- Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Client/Client.java
+++ Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Client/Client.java
@@ -3,9 +3,84 @@ package edu.colostate.cs.cs414.soggyZebras.rollerball.Client;
 import edu.colostate.cs.cs414.soggyZebras.rollerball.Game.Game;
 import edu.colostate.cs.cs414.soggyZebras.rollerball.Game.Location;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
 public class Client {
 
     // TODO: does Client need a game object?
+    boolean debug = false;
+
+    //========== NETWORK SETUP ==========//
+
+    //Local Networking Variables
+    String serverHost;
+    int serverPort;
+    Selector selector;
+
+    public Client(String serverAddress, int serverPort) throws IOException {
+
+        // Check if the arguments have been passed correctly
+        if (serverAddress == null){
+            throw new IOException("Invalid serverAddress");
+        }
+
+        // Try to assign given variables and create the selector
+        try{
+            this.serverHost = serverAddress;
+            this.serverPort = serverPort;
+            this.selector = Selector.open();
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    protected void initialize() throws IOException, InterruptedException {
+
+        // print debuggin info
+        if(debug){
+            System.out.println("Starting Client thread");
+        }
+
+        this.startClient();
+    }
+
+    private void startClient() throws IOException, InterruptedException {
+
+        // Create a socket for the client           [1]
+        // configure the socket for non-blocking    [2]
+        // register socket with the selector        [3]
+        // connect channel to server                [4]
+        // connect selector key to server           [5]
+
+        SocketChannel channel = SocketChannel.open();                           //[1]
+        channel.configureBlocking(false);                                       //[2]
+        channel.register(selector, SelectionKey.OP_CONNECT);                    //[3]
+        channel.connect(new InetSocketAddress(this.serverHost,this.serverPort));//[4]
+
+        selector.select();
+
+        Iterator<SelectionKey> keys = this.selector.selectedKeys().iterator();  //[5]
+        SelectionKey key = keys.next();
+        if(key.isConnectable()){
+            this.connect(key);
+        }
+
+    }
+
+    private void connect(SelectionKey key) throws IOException {
+        // finish setting up the connection to the server
+        SocketChannel channel = (SocketChannel) key.channel();
+        channel.finishConnect();
+        key.interestOps(SelectionKey.OP_WRITE);
+    }
+
+    //========= END NETWORK SETUP =========//
 
     public boolean makeMove(Location from, Location to) {
         return false;
--- Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Client/ClientTest.java
+++ Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Client/ClientTest.java
@@ -9,18 +9,19 @@ import java.io.IOException;
 
 class ClientTest {
 
-	@Test
-	void testConstructorNoArg() {
-
-		//Throw an exception if the Client class was created with no arguments
-		assertThrows(IOException.class, () -> {new Client();});
-	}
 
 	@Test
 	void testConstructorNullArg() throws IOException {
 
 		//Throw exception if the Client object was created wit null a argument(s)
-		Client c = new Client();
+		assertThrows(IOException.class, () -> {new Client(null,5000);});
+	}
+
+	@Test
+	void testCreateClientSelector() throws IOException {
+
+		//Thow an exception if the selector was not created correctly
+		Client c = new Client("127.0.0.1", 5000);
 	}
 
 }
