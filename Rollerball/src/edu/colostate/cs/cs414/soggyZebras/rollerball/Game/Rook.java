94b90a734c7aa5c42affb18ba0d959e2fd741c63
dawsoncanby <dawsoncanby@gmail.com>
2018-10-24 20:24:07 -0600

    started implementing rook

--- Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Game/Rook.java
+++ Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Game/Rook.java
@@ -1,8 +1,43 @@
 package edu.colostate.cs.cs414.soggyZebras.rollerball.Game;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Rook extends Piece {
 
     public Rook(Location l, char color, String type) {
         super(l, color, type);
+    }
+
+    public ArrayList<Location> validMoves(HashMap state) {
+        return new ArrayList<>();
+    }
+
+    public boolean externalRing() {
+        return loc.getRow() == 0 || loc.getCol() == 0 || loc.getRow() == 6 || loc.getCol() == 6;
+    }
+
+    /**
+     * get the current 'forward' direction of this piece
+     * @return
+     */
+    public Location getForward() {
+        // top of board
+        if ((loc.row == 0 && loc.col < 6)
+        || (loc.row == 1 && loc.col < 5 && loc.col > 0))
+            return new Location(0, 1);
+        // right side of board
+        if ((loc.col == 6 && loc.row < 6)
+            || (loc.col == 5 && loc.row < 5 && loc.row > 0))
+            return new Location(1, 0);
+        // bottom of board
+        if ((loc.row == 6 && loc.col > 0)
+            || (loc.row == 5 && loc.col > 1 && loc.col < 6))
+            return new Location(0, -1);
+        // left side of board
+        if ((loc.col == 0 && loc.row > 0)
+            || (loc.col == 1 && loc.row > 1 && loc.row < 6))
+            return new Location(-1, 0);
+        return new Location(0, 0);
     }
 }
--- Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Game/RookTest.java
+++ Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Game/RookTest.java
@@ -2,13 +2,55 @@ package edu.colostate.cs.cs414.soggyZebras.rollerball.Tests.Game;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import edu.colostate.cs.cs414.soggyZebras.rollerball.Game.Location;
+import edu.colostate.cs.cs414.soggyZebras.rollerball.Game.Rook;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RookTest {
+	Rook topOuter, topInner, leftOuter, leftInner,
+			bottomOuter, bottomInner, rightOuter, rightInner;
 
-	@Test
-	void test() {
-		assertEquals(true, true);
+	Location up, down, left, right;
+
+	@BeforeEach
+	public void init() {
+		topOuter = new Rook(new Location(0, 0), 'w', "rook");
+		topInner = new Rook(new Location(1, 1), 'w', "rook");
+		leftOuter = new Rook(new Location(6, 0), 'w', "rook");
+		leftInner = new Rook(new Location(5, 1), 'w', "rook");
+		bottomOuter = new Rook(new Location(6, 6), 'w', "rook");
+		bottomInner = new Rook(new Location(5, 5), 'w', "rook");
+		rightOuter = new Rook(new Location(0, 6), 'w', "rook");
+		rightInner = new Rook(new Location(1, 5), 'w', "rook");
+
+		up = new Location(-1, 0);
+		down = new Location(1, 0);
+		left = new Location(0, -1);
+		right = new Location(0, 1);
 	}
 
+	@Test
+	void testGetForwardTop() {
+		assertEquals(right, topOuter.getForward());
+		assertEquals(right, topInner.getForward());
+	}
+
+	@Test
+	void testGetForwardLeft() {
+		assertEquals(up, leftInner.getForward());
+		assertEquals(up, leftOuter.getForward());
+	}
+
+	@Test
+	void testGetForwardRight() {
+		assertEquals(down, rightOuter.getForward());
+		assertEquals(down, rightInner.getForward());
+	}
+
+	@Test
+	void testGetForwardBottom() {
+		assertEquals(left, bottomOuter.getForward());
+		assertEquals(left, bottomInner.getForward());
+	}
 }
