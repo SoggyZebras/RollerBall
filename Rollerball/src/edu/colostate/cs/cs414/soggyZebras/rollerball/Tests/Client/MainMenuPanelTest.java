060d479d957e5fd267ca4a6fd811aea83577dc90
dawsoncanby <dawsoncanby@gmail.com>
2018-11-04 10:48:33 -0700

    added main menu test class

---
+++ Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Tests/Client/MainMenuPanelTest.java
@@ -0,0 +1,12 @@
+package edu.colostate.cs.cs414.soggyZebras.rollerball.Tests.Client;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainMenuPanelTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
