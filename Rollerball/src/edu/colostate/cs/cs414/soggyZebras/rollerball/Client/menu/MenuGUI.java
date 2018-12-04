235b28443f0211977b1554f2e750f0b43175b45a
Jordan-Peterson <jordantp@rams.colostate.edu>
2018-12-04 10:55:49 -0700

    ips set for aws server

--- Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Client/menu/MenuGUI.java
+++ Rollerball/src/edu/colostate/cs/cs414/soggyZebras/rollerball/Client/menu/MenuGUI.java
@@ -43,7 +43,7 @@ public class MenuGUI extends JFrame {
 
         try {
             // TODO: change server address
-            client = new Client("127.0.0.1",35355);
+            client = new Client("3.16.167.10",35355);
             client.initialize();
             client.setGui(this);
         } catch (IOException e) {
